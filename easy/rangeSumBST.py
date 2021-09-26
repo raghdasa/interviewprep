fe6d5e98a794872f43699e9c1c5199b42d3fb383
radasa272 <raghuram.vmf15@gmail.com>
2021-09-26 15:57:35 -0700

    rangesumbst

---
+++ easy/rangeSumBST.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def rangeSumBST(self, root, low, high):
+        """
+        :type root: TreeNode
+        :type low: int
+        :type high: int
+        :rtype: int
+        """
+        ans = 0
+        stack = [root]
+        L= low
+        R = high
+        while stack:
+            node = stack.pop()
+            if node:
+                if L <= node.val <= R:
+                    ans += node.val
+                if L < node.val:
+                    stack.append(node.left)
+                if node.val < R:
+                    stack.append(node.right)
+        return ans
+
+#Complexity
+#O(n) where n is number of nodes in tree
+#O(n) length of stack is at worse case n
